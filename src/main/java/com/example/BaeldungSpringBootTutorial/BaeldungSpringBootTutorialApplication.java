ded3390a58f97639b2508f70d393ce102ffb5e22
kumouaSBD2 <ku.moua@sbd2.com>
2022-02-09 11:25:30 -0500

    Initial Commit - Following Baeldung Tutorial

---
+++ src/main/java/com/example/BaeldungSpringBootTutorial/BaeldungSpringBootTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.BaeldungSpringBootTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeldungSpringBootTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeldungSpringBootTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BaeldungSpringBootTutorial/BaeldungSpringBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BaeldungSpringBootTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeldungSpringBootTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
