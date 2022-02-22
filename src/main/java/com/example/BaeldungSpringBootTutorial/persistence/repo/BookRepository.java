c3c8c78face79a9f50c3979ba4a646fbe1cc792a
kumouaSBD2 <ku.moua@sbd2.com>
2022-02-22 16:36:04 -0500

    Practice commit/push

---
+++ src/main/java/com/example/BaeldungSpringBootTutorial/persistence/repo/BookRepository.java
@@ -0,0 +1,12 @@
+package com.example.BaeldungSpringBootTutorial.persistence.repo;
+
+import com.example.BaeldungSpringBootTutorial.persistence.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface BookRepository extends CrudRepository <Book, Long> {
+
+    List<Book> findByTitle(String title);
+
+}
